f314149c0ce4ab0901b2925c9d258dfcf1e97de3
Bohdan-Orlyk <lollikwinner@gmail.com>
2024-02-06 00:25:26 +0100

    refactoring

--- src/payments/api/payments.py
+++ src/payments/api/payments.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 
-from src.payments.services.payments_database_service import PaymentsDbService, Example1, Example2
 from src.payments.schemas import AllPaymentsSchema, PaymentSchema
 
 
@@ -11,7 +10,7 @@ payments_router = APIRouter(
 
 
 @payments_router.get("", response_model=AllPaymentsSchema)
-def get_payments_history():
+def get_payments_history() -> dict:
     """ Shows all payments """
 
     # payments: dict = get_payments()
@@ -21,7 +20,7 @@ def get_payments_history():
 
 
 @payments_router.get("/{payment_id}", response_model=PaymentSchema)
-def get_payment_by_id(payment_id: int):
+def get_payment_by_id(payment_id: int) -> dict:
     """ Shows payment by given `id` """
 
     # payment: dict = get_payments(payment_id: int, month: int)
@@ -31,7 +30,7 @@ def get_payment_by_id(payment_id: int):
 
 
 @payments_router.get("/{payments_month}", response_model=PaymentSchema)
-def get_payments_by_month(payment_month: int):
+def get_payments_by_month(payment_month: int) -> dict:
     """ Shows payment by `month` """
 
     # payment: dict = get_payments(month: int)
@@ -41,19 +40,19 @@ def get_payments_by_month(payment_month: int):
 
 
 @payments_router.post("")
-def book_payment_to_wallet():
+def book_payment_to_wallet() -> dict:
     """ Book the payment """
     pass
 
 
-@payments_router.patch("/{payment_id}")
-def update_wallet_booking():
+@payments_router.put("/{payment_id}")
+def update_wallet_booking() -> dict:
     """ Updates payment by `id` """
     pass
 
 
 @payments_router.delete("/{payment_id}")
-def delete_wallet_payment(payment_id: int):
+def delete_wallet_payment(payment_id: int) -> dict:
     """ Deletes payment by `id` """
     pass
 
