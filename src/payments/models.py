14033e968c69e9d6fde966af275f3e32f7564fac
Bohdan-Orlyk <lollikwinner@gmail.com>
2024-02-05 18:00:22 +0100

    added models for Payments

---
+++ src/payments/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, TIMESTAMP
+
+from datetime import datetime
+from database.db import Base
+
+
+def set_payment_date():
+    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Payment(Base):
+    __tablename__ = 'payment'
+    id = Column(Integer, primary_key=True, index=True)
+    price = Column(Float, nullable=False)
+    payment_date = Column(TIMESTAMP, default=set_payment_date)
+
+    payment_type = Column(Integer, ForeignKey('payment_types.id'))
+
+
+class PaymentTypes(Base):
+    __tablename__ = 'payment_types'
+    id = Column(Integer, primary_key=True)
+    type_name = Column(String)
+
+
