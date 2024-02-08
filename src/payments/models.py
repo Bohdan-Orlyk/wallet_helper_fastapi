c1f50285c858c07d96bbfe977a61bbc89c4e2a2b
Bohdan-Orlyk <lollikwinner@gmail.com>
2024-02-08 04:50:34 +0100

    refactoring

--- src/payments/models.py
+++ src/payments/models.py
@@ -5,20 +5,22 @@ from database.db import Base
 
 
 def set_payment_date():
-    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.strptime(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
 
 
 class Payment(Base):
     __tablename__ = 'payment'
+
     id = Column(Integer, primary_key=True, index=True)
     price = Column(Float, nullable=False)
     payment_date = Column(TIMESTAMP, default=set_payment_date)
 
-    payment_type = Column(Integer, ForeignKey('payment_types.id'))
+    payment_type = Column(String, ForeignKey('payment_types.id'),  nullable=False)
 
 
 class PaymentTypes(Base):
     __tablename__ = 'payment_types'
+
     id = Column(Integer, primary_key=True)
     type_name = Column(String)
 
